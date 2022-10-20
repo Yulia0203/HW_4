504d002ff8eb9ac50832e240d13d28d932aabd29
Yulia <mamaeva.jula@gmail.com>
2022-10-20 23:02:50 +0300

    first commit

---
+++ Task_4.2.py
@@ -0,0 +1,19 @@
+# Задача 1. Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+# 60 -> 2, 2, 3, 5
+
+
+number = int(input('Введите число N: '))
+
+def Factor(n):
+    list = []
+    for i in range(2, int(math.sqrt(n))+1):
+        while n % i == 0:
+            list.append(i)
+            n //= i
+        i += 1
+    if n > 1:
+        list.append(n)
+    return list
+import math
+
+print(f'Список простых множителей числа {number}: {Factor(number)}')
